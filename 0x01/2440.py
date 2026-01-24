9ff72082e84d7c09ea520d567b0f775405911c3c
OneMain1 <shindong97411@gmail.com>
2026-01-24 19:33:08 +0900

    2440

--- 0x01/2440.py
+++ 0x01/2440.py
@@ -6,3 +6,21 @@ while n > 0:
 
     print()
     n -= 1
+    
+# Solution 1 - range
+n = int(input())
+
+for i in range(n, 0, -1):
+    print("*" * i)
+    
+# Solution 2 - join
+n = int(input())
+
+for i in range(n, 0, -1):
+    print(''.join(['*'] * i))
+    
+# Solution 3 - reversed
+n = int(input())
+
+for i in reversed(range(1, n + 1)):
+    print("*" * i)
