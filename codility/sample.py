e34abc65b4d43d2c730ded8d8b32020033cd9332
OneMain1 <shindong97411@gmail.com>
2026-01-29 13:36:39 +0900

    sample

--- codility/sample.py
+++ codility/sample.py
@@ -18,3 +18,9 @@
 # N is an integer within the range [1..100,000];
 # each element of array A is an integer within the range [−1,000,000..1,000,000].
 
+def solution(A):                                                                                                                                
+    s = set(A)                                                                                                                                  
+    i = 1                                                                                                                                       
+    while i in s:                                                                                                                               
+        i += 1                                                                                                                                  
+    return i  
